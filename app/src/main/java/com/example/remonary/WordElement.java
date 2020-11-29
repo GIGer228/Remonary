f540bcd47dd2a8ff7626978f9a2f44354926f8b2
Boris <60959458+GIGer228@users.noreply.github.com>
2020-11-30 01:00:24 +0700

    Update WordElement.java
    Add equals() and hashCode() methods

--- app/src/main/java/com/example/remonary/WordElement.java
+++ app/src/main/java/com/example/remonary/WordElement.java
@@ -36,4 +36,24 @@ public class WordElement implements Serializable {
     public String getDescription() {
         return description;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WordElement that = (WordElement) o;
+
+        if (!title.equals(that.title)) return false;
+        if (!translate.equals(that.translate)) return false;
+        return description != null ? description.equals(that.description) : that.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + translate.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
 }
