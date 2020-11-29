045d888314f053d7e0ce05356d8bc8019e87654a
Boris <60959458+GIGer228@users.noreply.github.com>
2020-11-29 18:56:59 +0700

    Update NewWordActivity.java

--- app/src/main/java/com/example/remonary/NewWordActivity.java
+++ app/src/main/java/com/example/remonary/NewWordActivity.java
@@ -2,6 +2,8 @@ package com.example.remonary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +12,16 @@ import android.widget.Toast;
 
 public class NewWordActivity extends AppCompatActivity {
 
+    final String KEY_USER_WORDELEMENT = "user_word_element";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
 
-        EditText newWord = findViewById(R.id.new_word_text);
-        EditText newTranslate = findViewById(R.id.new_translation_text);
-        EditText newDescription = findViewById(R.id.new_description_text);
+        final EditText newWord = findViewById(R.id.new_word_text);
+        final EditText newTranslate = findViewById(R.id.new_translation_text);
+        final EditText newDescription = findViewById(R.id.new_description_text);
 
         Button addButton = findViewById(R.id.add_word_button);
         Button closeButton = findViewById(R.id.close_button);
@@ -25,6 +29,16 @@ public class NewWordActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                WordElement userWordElement = new WordElement();
+
+                userWordElement.setTitle(newWord.getText().toString());
+                userWordElement.setTranslate(newTranslate.getText().toString());
+                userWordElement.setDescription(newDescription.getText().toString());
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra(KEY_USER_WORDELEMENT, userWordElement);
+                setResult(Activity.RESULT_OK);
+
                 Toast.makeText(NewWordActivity.this, "Hmm... How interesting", Toast.LENGTH_SHORT).show();
                 finish();
             }
@@ -33,6 +47,8 @@ public class NewWordActivity extends AppCompatActivity {
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setResult(Activity.RESULT_CANCELED);
+
                 Toast.makeText(NewWordActivity.this, "Not funny. *gets anger*", Toast.LENGTH_SHORT).show();
                 finish();
             }
