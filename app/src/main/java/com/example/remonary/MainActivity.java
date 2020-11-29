2a7017e8d8cd967075738af31953d861bbd09a74
Boris <60959458+GIGer228@users.noreply.github.com>
2020-11-29 16:56:31 +0700

    Basic welcome screen design
    Create welcome screen and init objects in MainActivity.java

--- app/src/main/java/com/example/remonary/MainActivity.java
+++ app/src/main/java/com/example/remonary/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.remonary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeLine = findViewById(R.id.welcome_line);
+
+        Button newWordButton = findViewById(R.id.new_word_button);
+        Button dictionaryButton = findViewById(R.id.dictionary_button);
+        Button trainButton = findViewById(R.id.train_button);
+        Button optionsButton = findViewById(R.id.options_button);
+        Button exitButton = findViewById(R.id.exit_button);
+
+        newWordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        dictionaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        trainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
