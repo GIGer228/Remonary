ccee8d69e085eba9dba67c57077b8dc4318fa1f8
Boris <60959458+GIGer228@users.noreply.github.com>
2020-12-04 15:40:07 +0700

    Update WordListActivity.java

--- app/src/main/java/com/example/remonary/WordListActivity.java
+++ app/src/main/java/com/example/remonary/WordListActivity.java
@@ -1,17 +1,35 @@
 package com.example.remonary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class WordListActivity extends AppCompatActivity {
+import java.util.List;
+
+public class WordListActivity extends AppCompatActivity implements WordAdapterCallbacks{
+
+    private WordAdapter adapter;
+    private List<WordElement> dictionary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_list);
 
+        adapter = new WordAdapter(this);
+
         RecyclerView wordRecycler = findViewById(R.id.dictionary_recycler);
+
+        wordRecycler.setLayoutManager(new LinearLayoutManager(this));
+        wordRecycler.setAdapter(adapter);
+
+        adapter.setItems(dictionary);
+    }
+
+    @Override
+    public void onWordClick(WordElement word) {
+
     }
 }
