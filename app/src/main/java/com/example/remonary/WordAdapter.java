8c6623fd704ec02ccbd62f280fc06c5f6f9741f0
Boris <60959458+GIGer228@users.noreply.github.com>
2020-12-04 15:09:38 +0700

    Create WordAdapter.java

---
+++ app/src/main/java/com/example/remonary/WordAdapter.java
@@ -0,0 +1,50 @@
+package com.example.remonary;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
+import java.util.List;
+
+public class WordAdapter extends RecyclerView.Adapter<WordViewHolder> {
+
+    private List<WordElement> items = Collections.emptyList();
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.dictionary_item_view, parent, false);
+
+        return new WordViewHolder(view, new ViewHolderAction() {
+            @Override
+            public void action(int position) {
+                callbacks.onWordClick(items.get(position));
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        holder.bind(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return items.get(position).getId();
+    }
+
+    public void setItems(List<WordElement> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+}
