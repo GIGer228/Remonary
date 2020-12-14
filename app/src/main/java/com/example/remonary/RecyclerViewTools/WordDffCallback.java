a5ac20e22ce4ffe017f50b835c434403c51237c2
Boris <60959458+GIGer228@users.noreply.github.com>
2020-12-14 19:57:48 +0700

    Create WordDffCallback.java
    Srarting optimization for displaying words

---
+++ app/src/main/java/com/example/remonary/RecyclerViewTools/WordDffCallback.java
@@ -0,0 +1,37 @@
+package com.example.remonary.RecyclerViewTools;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import com.example.remonary.DataSet.WordElement;
+
+import java.util.List;
+
+public class WordDffCallback extends DiffUtil.Callback {
+
+    private List<WordElement> oldWords, newWords;
+
+    public WordDffCallback(List<WordElement> oldWords, List<WordElement> newWords) {
+        this.oldWords = oldWords;
+        this.newWords = newWords;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldWords.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newWords.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldWords.get(oldItemPosition) == newWords.get(newItemPosition);
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldWords.get(oldItemPosition).equals(newWords.get(newItemPosition));
+    }
+}
