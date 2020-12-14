ae2dc74f0fb4ff58de23c2aa40fd9bc743b87a70
Boris <60959458+GIGer228@users.noreply.github.com>
2020-12-14 21:50:22 +0700

    Import ItemTouchHelper to DictionaryActivity
    Imported ItemTouchHelper to help delete words from dictionary. There is also updateDictionary method. It will be init later

--- app/src/main/java/com/example/remonary/Activities/DictionaryActivity.java
+++ app/src/main/java/com/example/remonary/Activities/DictionaryActivity.java
@@ -1,18 +1,24 @@
 package com.example.remonary.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.remonary.DataSet.WordElement;
 import com.example.remonary.R;
 import com.example.remonary.RecyclerViewTools.WordAdapter;
 import com.example.remonary.RecyclerViewTools.WordAdapterCallbacks;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class DictionaryActivity extends AppCompatActivity implements WordAdapterCallbacks {
@@ -28,14 +34,38 @@ public class DictionaryActivity extends AppCompatActivity implements WordAdapter
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_list);
 
+        EditText emptyListMessage = findViewById(R.id.empty_message);
+
         dictionary = (List<WordElement>) getIntent().getExtras().get(MainActivity.KEY_USERDATA);
 
+        emptyListMessage.setVisibility(dictionary.size() == 0 ? View.VISIBLE : View.GONE);
+
         adapter = new WordAdapter(this);
+
+        ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @SuppressLint("NewApi")
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                dictionary.remove(position);
+                adapter.notifyItemRemoved(position);
+                MainActivity.updateDictionary(dictionary);
+            }
+        };
+
+        ItemTouchHelper swipeHelper = new ItemTouchHelper(callback);
 
         RecyclerView wordRecycler = findViewById(R.id.dictionary_recycler);
 
         wordRecycler.setLayoutManager(new LinearLayoutManager(this));
         wordRecycler.setAdapter(adapter);
+
+        swipeHelper.attachToRecyclerView(wordRecycler);
 
         adapter.setItems(dictionary);
     }
@@ -48,6 +78,7 @@ public class DictionaryActivity extends AppCompatActivity implements WordAdapter
         startActivityForResult(editWordIntent, RC_EDITWORD);
     }
 
+    @SuppressLint("NewApi")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == RC_EDITWORD && resultCode == Activity.RESULT_OK) {                        //replace old word with user_edit
@@ -57,6 +88,7 @@ public class DictionaryActivity extends AppCompatActivity implements WordAdapter
             dictionary.remove(editSource);
             dictionary.add(userEdit);
             adapter.notifyDataSetChanged();
+            MainActivity.updateDictionary(dictionary);
         }else super.onActivityResult(requestCode, resultCode, data);
     }
 }
